fe33553feb01e0c04c44031741b821aedda2d664
Eddie Jorden <eddiejorden.tech@gmail.com>
2021-04-12 11:44:22 -0700

    something something scope something i don't know

--- script.js
+++ script.js
@@ -4,26 +4,40 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
-const compareGuesses = (human, computer, target) => {
+const compareGuesses = (human) => {
 	let targetValue = Math.floor(Math.random() * 10);
+	console.log('targetValue', targetValue);
 
 	let computerGuess = Math.floor(Math.random() * 10);
+	console.log('computerGuess', computerGuess);
 
-	console.log('targetValue', targetValue);
 	let humanGuessValue = Math.abs(targetValue - human);
 	console.log('humanGuessValue', humanGuessValue);
 	console.log('human', human);
+
 	let computerGuessValue = Math.abs(targetValue - computerGuess);
-	console.log('computerGessValue', computerGuessValue);
+	console.log('computerGuessValue', computerGuessValue);
 	console.log('computerGuess', computerGuess);
+	console.log('humanScore', humanScore);
 
 	if (humanGuessValue > computerGuessValue) {
-		return 'the computer wins';
-	} else if (humanGuessValue < computerGuessValue) {
-		return 'the human wins';
+		return false;
+	}
+	if (humanGuessValue < computerGuessValue) {
+		return true;
 	} else {
-		return 'it is a miraculous tie';
+		return true;
 	}
 };
 
-console.log(compareGuesses(5));
+var compareGuessesResult = compareGuesses(5); /* global var*/
+
+const updateScore = (newVar) => {
+	if (compareGuessesResult) {
+		console.log('compareGuessResult', compareGuessesResult);
+		humanScore += 1;
+	} else {
+		computerScore += 1;
+	}
+	console.log('updateScore', updateScore);
+};
